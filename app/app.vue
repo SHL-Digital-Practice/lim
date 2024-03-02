74143f1e3c5a899016375767dbd3dfc803ed8954
Daniel Boba <62248969+DanielSBoba@users.noreply.github.com>
2024-03-02 16:59:07 +0100

    feat: supabase setup

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,3 @@
 <template>
-  <div class="underline">Hello</div>
+  <NuxtPage />
 </template>
---
+++ app/pages/catalogue.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    catalogue
+    {{ species }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { Database } from "../types/supabase";
+
+const db = useSupabaseClient<Database>();
+
+const { data: species, pending } = useAsyncData("species", async () => {
+  const { data, error } = await db.from("species").select("*");
+  console.log(data);
+
+  return data;
+});
+</script>
+
+<style scoped></style>
