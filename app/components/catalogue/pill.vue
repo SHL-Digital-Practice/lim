b3883a9736c528526b2a08a2b1f0ecc57da4cb1c
Daniel Boba <62248969+DanielSBoba@users.noreply.github.com>
2024-03-02 19:37:52 +0100

    feat: adding a pill

---
+++ app/components/catalogue/pill.vue
@@ -0,0 +1,17 @@
+<template>
+  <div
+    class="w-36 h-6 px-2.5 py-0.5 bg-emerald-100 rounded-xl justify-start items-center inline-flex"
+  >
+    <p class="text-emerald-800 text-sm font-medium leading-tight">
+      {{ label }}
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  label: string;
+}>();
+</script>
+
+<style scoped></style>
