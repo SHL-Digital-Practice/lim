a620e8f30e5492c4b5b49c3739aee1646f6f981f
Victor Wanderley Barbosa <vwb@shl.dk>
2024-03-02 13:29:58 +0100

    wip

---
+++ rhino/Catalogue.xaml.cs
@@ -0,0 +1,10 @@
+namespace LIM.Rhino.Views
+{
+  public partial class CataloguePanel
+  {
+    public CataloguePanel(uint documentSerialNumber)
+    {
+      InitializeComponent();
+    }
+  }
+}
---
+++ rhino/LIMPlugIn.cs
@@ -0,0 +1,26 @@
+﻿using Rhino.PlugIns;
+
+namespace LIM.Rhino
+{
+  public class LIMPlugIn : PlugIn
+  {
+
+    public LIMPlugIn()
+    {
+      Instance = this;
+    }
+
+    public static LIMPlugIn Instance
+    {
+      get; private set;
+    }
+
+    protected override LoadReturnCode OnLoad(ref string errorMessage)
+    {
+      return LoadReturnCode.Success;
+    }
+
+  }
+
+}
+
