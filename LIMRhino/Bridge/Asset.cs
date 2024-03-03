30c56edaf168c0bf78739c70df77047eaf2008ae
Daniel Boba <62248969+DanielSBoba@users.noreply.github.com>
2024-03-03 10:38:09 +0100

    fix: parsing fix

--- LIMRhino/Bridge/Asset.cs
+++ LIMRhino/Bridge/Asset.cs
@@ -4,20 +4,21 @@
 
     public class SpeciesRecord
     {
-        public int Id { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public string TaxonomicGroup { get; set; }
-        public string ScientificName { get; set; }
-        public string MapOfLifeLink { get; set; }
-        public string ThreatStatus { get; set; }
-        public string Family { get; set; }
-        public bool? Expected { get; set; }
-        public bool Recorded { get; set; }
-        public string CommonName { get; set; }
-        public string Obj { get; set; }
-        public string Image { get; set; }
-        public float? OxygenProductionKgPerAcrePerYear { get; set; }
-        public float? CarbonStorageKgPerAcrePerYear { get; set; }
+        public int Id { get; set; } // Non-nullable
+        public DateTime? CreatedAt { get; set; } // Nullable DateTime
+        public string? TaxonomicGroup { get; set; }
+        public string? ScientificName { get; set; }
+        public string? MapOfLifeLink { get; set; }
+        public string? ThreatStatus { get; set; }
+        public string? Family { get; set; }
+        public string? Expected { get; set; }
+        public string? Recorded { get; set; }
+        public string? CommonName { get; set; }
+        public string? Obj { get; set; }
+        public string? Image { get; set; }
+        public double? OxygenProductionKgPerAcreYear { get; set; } // Nullable double
+        public double? CarbonStorageKgPerAcreYear { get; set; } // Nullable double
+
     }
 
 }
